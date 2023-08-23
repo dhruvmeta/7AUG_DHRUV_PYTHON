622efa54870fffc28b14d5972eb4ff5d11151dac
dhruvmeta <dhruvmeta0@gmail.com>
2023-08-23 11:49:24 +0530

    data

---
+++ Module-1/multipale_table.py
@@ -0,0 +1,7 @@
+number_of_tables = cint(input("How many number of tables you want to print!?"))
+
+for a in range(0,number_of_tables):
+    n=int(input(f"enter table {a} value:"))
+
+    for i in range (1,11):
+        print(f"{n} * {1} = {n*i}")
